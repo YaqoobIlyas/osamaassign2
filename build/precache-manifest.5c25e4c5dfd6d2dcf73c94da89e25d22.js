a1d5a3d94e671db763540202a2be9dfa0bc5ab33
Yaqoob Ilyas <yaqoobilyas786@gmail.com>
2023-05-27 23:19:33 +0500

    first commit

---
+++ build/precache-manifest.5c25e4c5dfd6d2dcf73c94da89e25d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4313d3d3686c0b601cc883fb834a36f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9968a69c0ece20989a2e",
+    "url": "/static/css/2.4e80ca6c.chunk.css"
+  },
+  {
+    "revision": "b9d5da30db921e1b53e0",
+    "url": "/static/css/main.11306bcb.chunk.css"
+  },
+  {
+    "revision": "9968a69c0ece20989a2e",
+    "url": "/static/js/2.4e74ba9e.chunk.js"
+  },
+  {
+    "revision": "b9d5da30db921e1b53e0",
+    "url": "/static/js/main.9204054b.chunk.js"
+  },
+  {
+    "revision": "5caba256003187cee59a",
+    "url": "/static/js/runtime-main.6e809d35.js"
+  }
+]);
